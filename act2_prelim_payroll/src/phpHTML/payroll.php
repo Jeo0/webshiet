86990d1939008e6ac9d7957a99f9f077aee59ab1
Jeo0 <jjrelb@gmail.com>
2025-09-06 15:07:32 +0800

    passed

--- act2_prelim_payroll/src/phpHTML/payroll.php
+++ act2_prelim_payroll/src/phpHTML/payroll.php
@@ -10,7 +10,6 @@
 
         <!-- custom styles (relative path) -->
         <link rel="stylesheet" href="<?php echo "$g_root_location" . "src/css/image_card_styles.css"; ?>" >
-        <link rel="stylesheet" href="<?php echo "$g_root_location" . "src/css/button_styles.css"; ?>" >
 
         <!-- webpage selector -->
         <script src="
@@ -37,8 +36,9 @@
 //    [3][1][2]   button text
 //    ...
 $payrollindexes = array( "EMPLOYEE BASIC INFO",  
-                            [["Employee Number",        "input"],
-                            ["Search Employee",         "button", "GADAM"],
+                            [["cat bleh",               "image"],
+                            ["Employee Number",         "input"],
+                            ["Search Employee",         "button", "Search"],
                             ["Department",              "input disabled"]],
                         "BASIC INCOME",
                             [["Rate / Hour",            "input"],
@@ -54,8 +54,12 @@ $payrollindexes = array( "EMPLOYEE BASIC INFO",
                             ["Income / Cut Off",        "input disabled"]],
                         "SUMMARY INCOME",
                             [["GROSS INCOME",   "input"],
-                            ["NET INCOME",      "input disables"],
-                            ["Firstname",       "input disabled"],
+                            ["NET INCOME",      "input disabled"]]
+);
+
+$second_form = array(
+                        "SUMMARY INCOME",
+                            [["Firstname",       "input disabled"],
                             ["Middle Name",     "input disabled"],
                             ["Surname",         "input disabled"],
                             ["Civil Status",    "input disabled"],
@@ -75,8 +79,7 @@ $payrollindexes = array( "EMPLOYEE BASIC INFO",
                             ["Faculty Savings Loan",    "input"],
                             ["Salary Loan",             "input"],
                             ["Other Loans",             "input"]],
-                        "DEDUCTION SUMMARY",
-                            [["Total Deductions",       "input disabled"]]
+                        "DEDUCTION SUMMARY"
 );
 
 
@@ -84,52 +87,75 @@ $payrollindexes = array( "EMPLOYEE BASIC INFO",
 ?>
 
 
-    <body>
+    <body style="background-color: #f4f4f4;" class="container-flex m-2 ">
 
-        <div class="container py-5">
+        <!-- mismong form -->
+        <div class="container-flex m-5 bg-white mismongForm" style="box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);">
+  
+
 
             <!-- start of -->
-            <div class="row g-3">
-
-                <!-- Order Details Column -->
-                <div class="col-auto col-md-5">
-
+            <div class="row gx-0 justify-content-center">
 
 <?php
+// Order Details 1 Column 
+echo '<div class="col-auto col-md-5">';
+
+
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+///////////////////////////////////////////
+// first column
+
 $i_title_index_division = 0;
 foreach($payrollindexes as $i_title){
 
     if ($i_title_index_division % 2 == 0) {// if even
-        echo "<div class='card my-0'>";                                                                            /////// card
-        echo '<h5 class="fw-bold mt-4">' . $i_title . '</h5><br>';  // EMPLOYEE BASIC INFO, BASIC INCOME, ETC
+        echo "<div class='card m-2 bg-light border-0'>";                                                                            /////// card
+        echo "<div class='m-2'>";                                                                                  /////// padding on the left
+
+        echo '<h5 class="fw-bold mt-2">' . $i_title . '</h5>';  // EMPLOYEE BASIC INFO, BASIC INCOME, ETC
     }
 
     else {  // if array and odd
         // access and print the each 
         // current context is $i_title is now an array that contains 3 or n arrays: [], [], []
+
         for($i_label=0; $i_label<count($i_title); $i_label++){
             
             $e_current_context = $i_title[$i_label];   // array context 
 
+
+
                 // print the label text
-                echo $e_current_context[0] . ":" . "<br>";
+                // SEARCH EMPLOYEE
+                if($e_current_context[0] != "cat bleh")
+                  echo $e_current_context[0] . ":";
+                
+                
+
 
                 // establish the rules
+                // INPUTS
                 switch($e_current_context[1]){
                     case "input disabled":
-                        echo '<input type="text" name="price" id="price" class="form-control " disabled>' . "<br>";
+                        echo '<input type="text" name="price" id="price" class="form-control " disabled>';
                         break;
 
                     case "input":
-                        echo '<input type="text" name="price" id="price" class="form-control ">' . "<br>";
+                        echo '<input type="text" name="price" id="price" class="form-control ">';
                         break;
 
                     case "button":
-                        echo '<button class="btn btn-danger flex-fill">' 
+                        echo '<button type="button" class="btn btn-danger flex-fill d-block">' 
                             . $e_current_context[2]
-                            . '</button>'
-                            . '<br>';
+                            . '</button>';
                         break;
+                    case "image":
+                        echo '<img src="../../assets/cat bleh.png" '
+                          . 'class="img d-block" ';
                 }
                 
             }
@@ -138,55 +164,110 @@ foreach($payrollindexes as $i_title){
 
     $i_title_index_division++;
     if ($i_title_index_division % 2 == 0) {// if even
+        echo "</div>";                                                                                  /////// padding left
         echo "</div>";                                                                                  /////// card
     }
 }
 
+echo '</div>'; // end of column
+
+echo '<div class="col-auto col-md-5">'; // another column
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+// second column
+$i_title_index_division = 0;
+foreach($second_form as $i_title){
+
+    if ($i_title_index_division % 2 == 0) {// if even
+        echo "<div class='card m-2 bg-light border-0'>";                                                                            /////// card
+        echo "<div class='m-2'>";                                                                                  /////// padding on the left
+
+        echo '<h5 class="fw-bold my-2">' . $i_title . '</h5>';  // EMPLOYEE BASIC INFO, BASIC INCOME, ETC
+
+
+        // <!--FINAL COLUMN -->
+        // hardcoded
+        if($i_title == "DEDUCTION SUMMARY"){
+            echo 'Total Deductions:';
+            echo '<input type="text" name="price" id="price" class="form-control mb-3" disabled>';
+            
+            // <!-- top Buttons -->
+            echo '<div class="mb-3 d-flex gap-1 flex-wrap">';
+                echo '<button class="btn btn-primary flex-fill">GROSS INCOME</button>';
+                echo '<button class="btn btn-primary flex-fill">NET INCOME</button>';
+                echo '<button class="btn btn-info flex-fill">SAVE</button>';
+                echo '<button class="btn btn-info flex-fill">UPDATE</button>';
+                echo '<button class="btn btn-warning flex-fill">NEW</button>';
+            echo '</div>';
+        }
+
+    }
+
+    else {  
+
+      // if array and odd
+        // access and print the each 
+        // current context is $i_title is now an array that contains 3 or n arrays: [], [], []
+        for($i_label=0; $i_label<count($i_title); $i_label++){
+            
+            $e_current_context = $i_title[$i_label];   // array context 
+
+
+
+                // print the label text
+                // SEARCH EMPLOYEE
+                if($e_current_context[0] != "cat bleh")
+                  echo $e_current_context[0] . ":";
+
+                
+
+
+                // establish the rules
+                // INPUTS
+                switch($e_current_context[1]){
+                    case "input disabled":
+                        echo '<input type="text" name="price" id="price" class="form-control " disabled>';
+                        break;
+
+                    case "input":
+                        echo '<input type="text" name="price" id="price" class="form-control ">';
+                        break;
+
+                    case "button":
+                        echo '<button type="button" class="btn btn-danger flex-fill d-block">' 
+                            . $e_current_context[2]
+                            . '</button>';
+                        break;
+                    case "image":
+                        echo '<img src="../../assets/cat bleh.png" '
+                          . 'class="img d-block" ';
+                }
+                
+            }
+        }
+    
+
+    $i_title_index_division++;
+    if ($i_title_index_division % 2 == 0) {// if even
+        echo "</div>";                                                                                  /////// padding left
+        echo "</div>";                                                                                  /////// card
+    }
+}
+
+
+
 ?>
-
-
-                <!--
-                    <h5 class="fw-bold mt-4">Order Details:</h5>
-
-                    <div class="d-flex flex-column gap-2">
-
-
-                        <div class="input_box  col-auto">
-                            <span>Item Name:</span>
-                            </div>
-                            <div class="col-auto">
-                            <input type="text" name="item_name" id="item_name" class="form-control form-control-sm" >
-                        </div>
-
-                        <div class="input_box col-auto">
-                            <span>Price:</span>
-                            </div>
-                            <div class="col-auto">
-                            <input type="text" name="price" id="price" class="form-control " disabled>
-                        </div>
-
-                    </div>
-                -->
                 </div>
 
 
 
-                <!-- Discount Options + Calculator Column -->
-                <div class="col-auto col-md-5">
-                    <h5 class="fw-bold mb-3">Order Discount Options:</h5>
-
-                    <!-- top Buttons -->
-                    <div class="mb-3 d-flex gap-2 flex-wrap">
-                        <button class="btn btn-primary flex-fill">CALCULATE CHANGE</button>
-                        <button class="btn btn-danger flex-fill">NEW</button>
-                        <button class="btn btn-warning flex-fill">SAVE</button>
-                        <button class="btn btn-secondary flex-fill">UPDATE</button>
-                    </div>
-
-                </div>
 
 
-            </div>
+            </div>    
+        
         </div>
 
 
